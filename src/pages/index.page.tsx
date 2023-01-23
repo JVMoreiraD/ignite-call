b9dfbb5588b68537fc518c87fafe89a0e9b2c57b
jvmoreirad <jvitormoreirad@hotmail.com>
2023-01-23 20:14:22 -0300

    feat: setup e estrutura

---
+++ src/pages/index.page.tsx
@@ -0,0 +1,8 @@
+import { globalStyles } from '@/styles/global'
+import { Heading } from '@ignite-ui/react'
+
+globalStyles()
+
+export default function Home() {
+  return <Heading>Hello World</Heading>
+}
